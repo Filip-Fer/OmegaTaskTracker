7e29e51767018e559c2d78bd7c7892ced18887bb
Filip-Fer <filip.ferencei@gmail.com>
2023-03-24 10:26:54 +0100

    Few linex added

---
+++ library.php
@@ -0,0 +1,21 @@
+<?php
+function getHead(){
+    include 'head.php';
+}
+
+function getFooter(){
+    include 'footer.php';
+}
+
+function getDB(){
+    include 'database.php';
+}
+
+function getContent(){
+    include 'product.php';
+}
+
+function getEncryption(){
+    include 'pswencryption.php';
+}
+?>
